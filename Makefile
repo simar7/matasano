123bbf26257345e6a461004649840a8cbbe1213b
Simarpreet Singh <s244sing@uwaterloo.ca>
2016-05-03 05:02:15 -0400

    Adding linux-x64 code and updated makefile
    Signed-off-by: Simarpreet Singh <s244sing@uwaterloo.ca>

--- Makefile
+++ Makefile
@@ -1,10 +1,13 @@
 CC = gcc
-CFLAGS = -g -Wall -lcrypto
+CFLAGS = -g -Wall
+OPENSSL_CFLAGS = -lssl -lcrypto
 SRC_DIR = src
 BUILD_DIR = bin
+INCLUDE_DIR = /usr/local/ssl/include
+LIB_DIR = /usr/local/ssl/lib
 
 c1: $(SRC_DIR)/set1/c1.c
-	$(CC) $(CFLAGS) $(SRC_DIR)/set1/c1.c -o $(BUILD_DIR)/set1/c1
+	$(CC) $(CFLAGS) $(SRC_DIR)/set1/c1.c -o $(BUILD_DIR)/set1/c1 $(OPENSSL_CFLAGS) -I$(INCLUDE_DIR) -L$(LIB_DIR)  
 
 clean:
-	rm -rf $(BUILD_DIR)/*
+	rm -rf $(BUILD_DIR)/set1/*
--- src/set1/c1.c
+++ src/set1/c1.c
@@ -2,6 +2,7 @@
 #include <stdlib.h>
 #include <string.h>
 #include <openssl/bio.h>
+#include <openssl/evp.h>
 
 #define SIZE 1024
 #define INPUT_HEX_STR "49276d206b696c6c696e6720796f757220627261696e206c696b65206120706f69736f6e6f7573206d757368726f6f6d"
@@ -11,14 +12,19 @@ char* _hex_to_base64(char* input_string) {
     BIO *bio, *b64;
     char output_string[SIZE];
 
+    bio = BIO_new(BIO_s_mem());
     b64 = BIO_new(BIO_f_base64());
-    bio = BIO_new_fp(stdout, BIO_NOCLOSE);
-
+    
     BIO_push(b64, bio);
-    BIO_write(b64, INPUT_HEX_STR, strlen((char *)INPUT_HEX_STR));
+    BIO_write(b64, input_string, strlen(input_string));
     BIO_flush(b64);
+    
+    BIO_read(bio, output_string, strlen(input_string)); 
 
-    return (char*) OUTPUT_BASE64_STR;
+    BIO_free(bio);
+    BIO_free(b64);
+
+    return (char*) output_string;
 }
 
 int main(int argc, char *argv[]) {
@@ -27,8 +33,12 @@ int main(int argc, char *argv[]) {
 
     char* output_string = _hex_to_base64(input_string);
     if (!strcmp(output_string, OUTPUT_BASE64_STR)) {
-        printf("%s", "Strings match!");
+        printf("%s", "Strings match!\n");
+    } else {
+        printf("%s", "Strings don't match!\n");
+        printf("Resulting output = %s\n", output_string);
+        printf("Desired   output = %s\n", OUTPUT_BASE64_STR);
+        return -1;
     }
-
     return 0;
 }
